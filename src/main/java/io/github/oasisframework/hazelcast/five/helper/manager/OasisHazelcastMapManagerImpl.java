d3ec53b90aab03cfceee5fb6dce00233153e885d
muhsincaliskan <muhsincaliskan96@gmail.com>
2024-08-21 18:22:26 +0300

    implemented map manager methods

--- src/main/java/io/github/oasisframework/hazelcast/five/helper/manager/OasisHazelcastMapManagerImpl.java
+++ src/main/java/io/github/oasisframework/hazelcast/five/helper/manager/OasisHazelcastMapManagerImpl.java
@@ -2,9 +2,11 @@ package io.github.oasisframework.hazelcast.five.helper.manager;
 
 import com.hazelcast.core.HazelcastInstance;
 import io.github.oasisframework.hazelcast.common.manager.OasisHazelcastMapManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class OasisHazelcastMapManagerImpl implements OasisHazelcastMapManager {
 	private final HazelcastInstance hazelcastInstance;
 
@@ -14,11 +16,21 @@ public class OasisHazelcastMapManagerImpl implements OasisHazelcastMapManager {
 
 	@Override
 	public <K, V> V getValueFromMap(String mapName, K key) {
-		return null;
+
+		return (V) hazelcastInstance.getMap(mapName).get(key);
+
 	}
 
 	@Override
 	public <K, V> void addValueToMap(String mapName, K key, V value) {
-
+		hazelcastInstance.getMap(mapName).set(key, value);
+	}
+	public boolean contains(String mapName, String key){
+		try{
+			return hazelcastInstance.getMap(mapName).containsKey(key);
+		}catch (RuntimeException ex){
+			log.error("Exception occurred contains(mapName, key). Map: {}, Key: {}", mapName, key);
+			return false;
+		}
 	}
 }
