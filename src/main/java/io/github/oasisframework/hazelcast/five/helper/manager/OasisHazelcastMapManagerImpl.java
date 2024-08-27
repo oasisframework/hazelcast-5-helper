a35f7745a1f544953275029aa2f7f84b637f5eef
Berkay Yelken <berkayelken@gmail.com>
2024-08-27 14:47:57 +0300

    Update OasisHazelcastMapManagerImpl.java

--- src/main/java/io/github/oasisframework/hazelcast/five/helper/manager/OasisHazelcastMapManagerImpl.java
+++ src/main/java/io/github/oasisframework/hazelcast/five/helper/manager/OasisHazelcastMapManagerImpl.java
@@ -1,12 +1,11 @@
 package io.github.oasisframework.hazelcast.five.helper.manager;
 
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
 import io.github.oasisframework.hazelcast.common.manager.OasisHazelcastMapManager;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 public class OasisHazelcastMapManagerImpl implements OasisHazelcastMapManager {
 	private final HazelcastInstance hazelcastInstance;
 
@@ -16,20 +15,20 @@ public class OasisHazelcastMapManagerImpl implements OasisHazelcastMapManager {
 
 	@Override
 	public <K, V> V getValueFromMap(String mapName, K key) {
-
-		return (V) hazelcastInstance.getMap(mapName).get(key);
-
+		IMap<K,V> map = hazelcastInstance.getMap(mapName);
+		return map.get(key);
 	}
 
 	@Override
 	public <K, V> void addValueToMap(String mapName, K key, V value) {
 		hazelcastInstance.getMap(mapName).set(key, value);
 	}
+
 	public boolean contains(String mapName, String key){
 		try{
-			return hazelcastInstance.getMap(mapName).containsKey(key);
+			IMap<?,?> map = hazelcastInstance.getMap(mapName);
+			return map.containsKey(key);
 		}catch (RuntimeException ex){
-			log.error("Exception occurred contains(mapName, key). Map: {}, Key: {}", mapName, key);
 			return false;
 		}
 	}
